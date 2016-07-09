3495df5eb1bfdc109aa23f297e83292755f882da
dselvam1986 <dineshse1986@gmail.com>
2016-07-08 23:57:50 -0400

    adding change Element by id

---
+++ component/changeValueWithButtonClick/js/changeElement.js
@@ -0,0 +1,7 @@
+function myFunction() {
+  $("div#changer > p").replaceWith("<p>Hello</p>");
+}
+
+function txtRevert() {
+  $("div#changer > p").replaceWith("<p>Click the button to Change text</p>");
+}
